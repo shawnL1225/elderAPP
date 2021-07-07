d43ec388bcf6f2922fe0a0ff2f18aee45d9f6388
shawnL <shawnL@csie.io>
2021-07-07 22:29:10 +0800

    login with database user table & determine fail problem

--- app/src/main/java/com/example/elderapp/LoginActivity.java
+++ app/src/main/java/com/example/elderapp/LoginActivity.java
@@ -4,32 +4,76 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
-import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class LoginActivity extends AppCompatActivity{
+
+    String account, password;
+    EditText etAccount, etPassword;
+    private String url = "https://www2.cs.ccu.edu.tw/~lwx109u/elderApp/login.php";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        Button btn_login = (Button)findViewById(R.id.btn_login);
-        Button btn_regist = (Button)findViewById(R.id.btn_regist);
-        btn_login.setOnClickListener(this);
-        btn_regist.setOnClickListener(this);
+
+        etAccount = findViewById(R.id.et_account);
+        etPassword = findViewById(R.id.et_password);
     }
 
-    @Override
-    public void onClick(View v) {
-        switch(v.getId()){
-            case R.id.btn_login:
-                Intent volunteer = new Intent(LoginActivity.this, VolunteerActivity.class);
-                startActivity(volunteer);
-                break;
-            case R.id.btn_regist:
-                Intent signup = new Intent(LoginActivity.this, SignUpActivity.class);
-                startActivity(signup);
-                break;
+
+
+
+    public void register(View view){
+        Intent it = new Intent(LoginActivity.this, RegisterActivity.class);
+        startActivity(it);
+        finish();
+    }
+
+    public void login(View view) {
+        account = etAccount.getText().toString().trim();
+        password = etPassword.getText().toString().trim();
+
+        if(!account.equals("")){
+            StringRequest stringRequest = new StringRequest(Request.Method.POST, url, response -> {
+                Log.d("connect", "Response: "+response);
+                if (response.equals("success")) {
+                    Intent it = new Intent(LoginActivity.this, VolunteerActivity.class);
+                    startActivity(it);
+                    finish();
+                } else if (response.equals("NoUser")) {
+                    Toast.makeText(LoginActivity.this, "登入失敗 - 查無此使用者", Toast.LENGTH_SHORT).show();
+                }else if (response.equals("WrongPass")) {
+                    Toast.makeText(LoginActivity.this, "登入失敗 - 密碼錯誤", Toast.LENGTH_SHORT).show();
+                }
+
+            }, error -> {
+                Toast.makeText(LoginActivity.this, error.toString().trim(), Toast.LENGTH_SHORT).show();
+            }){
+                @Override
+                protected Map<String, String> getParams() {
+                    Map<String, String> data = new HashMap<>();
+                    data.put("phone", account);
+                    data.put("password", password);
+                    return data;
+                }
+            };
+            RequestQueue requestQueue = Volley.newRequestQueue(this);
+            requestQueue.add(stringRequest);
+
+        }else{
+            Toast.makeText(this, "請輸入帳號", Toast.LENGTH_SHORT).show();
         }
     }
 }
