8c9f47c83239d6f7167c632fc2c150944fea6fac
shawnL <shawnL@csie.io>
2021-10-26 14:05:59 +0800

    part of font size & goout checkout background

--- app/src/main/java/com/example/elderapp/SplashActivity.kt
+++ app/src/main/java/com/example/elderapp/SplashActivity.kt
@@ -3,15 +3,24 @@ package com.example.elderapp
 import android.animation.Animator
 import android.animation.AnimatorListenerAdapter
 import android.content.Intent
+import android.content.res.Configuration
 import android.os.Bundle
 import android.os.Handler
 import android.view.View
 import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
+import android.view.WindowManager
+import android.util.Log
+
+
+
+
+
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        adjustFontScale(resources.configuration);
         setContentView(R.layout.activity_splash)
 
         val logo = findViewById<ImageView?>(R.id.img_Logo)
@@ -28,9 +37,7 @@ class SplashActivity : AppCompatActivity() {
             finish()
         }, 3000)
 
-//        Intent it = new Intent(SplashActivity.this, LoginActivity.class);
-//        startActivity(it);
-//        finish();
+
     }
 
     private fun animateIn(view: View?) {
@@ -51,5 +58,20 @@ class SplashActivity : AppCompatActivity() {
                         view.visibility = View.GONE
                     }
                 })
+    }
+
+    private fun adjustFontScale(configuration: Configuration) {
+        if (configuration.fontScale > 1.0) {
+            Log.d("system",
+                "fontScale=" + configuration.fontScale) //Custom Log class, you can use Log.w
+            Log.d("system",
+                "font too big. scale down...") //Custom Log class, you can use Log.w
+            configuration.fontScale = 1.0f
+            val metrics = resources.displayMetrics
+            val wm = getSystemService(WINDOW_SERVICE) as WindowManager
+            wm.defaultDisplay.getMetrics(metrics)
+            metrics.scaledDensity = configuration.fontScale * metrics.density
+            baseContext.resources.updateConfiguration(configuration, metrics)
+        }
     }
 }
