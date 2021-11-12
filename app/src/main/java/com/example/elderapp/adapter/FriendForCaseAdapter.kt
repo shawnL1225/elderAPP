c8b5c92c65d193d5170cbb2f595ce53ad8707222
lyonlu13 <lyonlu13@gmail.com>
2021-11-12 15:53:30 +0800

    Go out case issue fixing and new feature

--- app/src/main/java/com/example/elderapp/adapter/FriendForCaseAdapter.kt
+++ app/src/main/java/com/example/elderapp/adapter/FriendForCaseAdapter.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.recyclerview.widget.RecyclerView
 import com.example.elderapp.Global
@@ -15,6 +16,11 @@ class FriendForCaseAdapter(private val context: Context, private var data: Mutab
 
     class SelectableUser(user:User): User(user.id,user.name,user.phone) {
         var selected = false
+
+        fun head(headshot:String) :SelectableUser {
+            this.headshot = headshot
+            return this
+        }
     }
 
 
